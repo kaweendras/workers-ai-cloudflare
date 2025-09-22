f7e2e54e93f4e681d90c09c886643a3f47ddebe4
kaweendras <salitha.wijerathna@gmail.com>
2025-09-22 15:17:43 +0530

    CHG | enhance authentication middleware with admin role check and token decoding functionality

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -23,3 +23,42 @@ export const authMiddleware = (
 
   next();
 };
+
+export const adminMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    console.log(`No token provided`);
+    return res.status(401).json({
+      message: "No token provided User must logged in to do this action",
+    });
+  }
+  const { valid, decoded, error } = verifyToken(token);
+  if (!valid) {
+    console.log(`Invalid token: ${error}`);
+    return res.status(401).json({ message: "Invalid token", error });
+  }
+  const payload = decoded as JwtPayload;
+  if (payload.role !== "admin") {
+    console.log(`User is not admin`);
+    return res.status(403).json({ message: "Access denied, admin only" });
+  }
+  next();
+};
+
+export const decodedToken = (req: Request): JwtPayload | null => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    console.log(`No token provided`);
+    return null;
+  }
+  const { valid, decoded, error } = verifyToken(token);
+  if (!valid) {
+    console.log(`Invalid token: ${error}`);
+    return null;
+  }
+  return decoded as JwtPayload;
+};
